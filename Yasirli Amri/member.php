e2f7e5103d23baecef18da9d468a5d6858ec7bb8
muhammadsadry19@gmail.com <muhammadsadry19@gmail.com>
2022-06-15 20:52:19 +0700

    footer

--- Yasirli Amri/member.php
+++ Yasirli Amri/member.php
@@ -26,25 +26,25 @@ if (!isset($_SESSION["login"])){
     <title>Member</title>
 
     <!-- Vendor CSS Files -->
-    <link href="vendor/aos/aos.css" rel="stylesheet" />
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
-    <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
-    <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
-    <link href="vendor/remixicon/remixicon.css" rel="stylesheet" />
-    <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+    <link href="./vendor/aos/aos.css" rel="stylesheet" />
+    <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+    <link href="./vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+    <link href="./vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+    <link href="./vendor/remixicon/remixicon.css" rel="stylesheet" />
+    <link href="./vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
 
-    <link href="css/mdb.min.css?ver=1.2.1" rel="stylesheet" />
-    <link href="css/aos.css?ver=1.2.1" rel="stylesheet" />
-    <link href="css/main.css?ver=1.2.1" rel="stylesheet" />
-    <link href="css/button.css" rel="stylesheet" />
-    <link href="css/style.css" rel="stylesheet" />
+    <link href="./css/mdb.min.css?ver=1.2.1" rel="stylesheet" />
+    <link href="./css/aos.css?ver=1.2.1" rel="stylesheet" />
+    <link href="./css/main.css?ver=1.2.1" rel="stylesheet" />
+    <link href="./css/button.css" rel="stylesheet" />
+    <!-- <link href="./css/style.css" rel="stylesheet" /> -->
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
 
     <!-- Template Main CSS File -->
-    <link href="css/member.css" rel="stylesheet" />
+    <link href="./css/member.css" rel="stylesheet" />
   </head>
   <body>
     <!-- ======= Header ======= -->
@@ -306,6 +306,6 @@ if (!isset($_SESSION["login"])){
     <script src="vendor/swiper/swiper-bundle.min.js"></script>
     <script src="vendor/php-email-form/validate.js"></script>
 
-    <script src="js/member.js"></script>
+    <script src="./js/member.js"></script>
   </body>
 </html>
